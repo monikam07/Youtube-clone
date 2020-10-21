980f414a69ee3d4250dbac0585325eccfbe16ce0
unknown <monikam0798@gmail.com>
2020-10-21 09:46:56 -0700

    first commit

---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0rZhnyvmmKWUNGnMxYwOuQVRR7BLlZeE",
+    authDomain: "clone-79c1a.firebaseapp.com",
+    databaseURL: "https://clone-79c1a.firebaseio.com",
+    projectId: "clone-79c1a",
+    storageBucket: "clone-79c1a.appspot.com",
+    messagingSenderId: "251503998730",
+    appId: "1:251503998730:web:d3e0f793c35aa96f296cf3",
+    measurementId: "G-8FDVJRS9LF"
+  };
